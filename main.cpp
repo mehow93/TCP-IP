dd38cc6a756488d9d6c03dc15bcd6ba6c82847ae
mehow93 <michmad93@gmail.com'git config --global user.email michmad93@gmail.comdsfbvsecho penisgit config --global user.email "michmad93@gmail.com>
2019-03-23 15:22:33 +0100

    addresses ready

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <sys/socket.h>
+#include <sys/socket.h> // anable using socks
+#include <netinet/in.h>  // anable using structsockaddr_in
 #define PORT 2399
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
-
+    int sockfd = socket(AF_INET,SOCK_STREAM,0);
+    struct sockaddr_in address; // adres
+    struct sockaddr_in serv_addr; // server address
     return 0;
 }
