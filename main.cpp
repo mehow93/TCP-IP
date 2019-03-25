b799ab1d89a701c09d645739b6f21280266857e9
knopers8 <knopers8@op.pl>
2019-03-25 14:55:06 +0100

    Better order of the code

--- main.cpp
+++ main.cpp
@@ -10,23 +10,21 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
-    int sock_fd = socket(AF_INET,SOCK_STREAM,0);
 
-    struct sockaddr_in address; // adres
     struct sockaddr_in serv_addr; // server address
-    char msg []= "Hello Server";
-
-
-
     memset(&serv_addr,'0',sizeof(serv_addr));
-
     serv_addr.sin_family = AF_INET; // IPv4 protocol
     serv_addr.sin_port = htons(PORT); // htons convert PORT to network order
     inet_pton(AF_INET,"127.0.0.1",&serv_addr.sin_addr); // convert do adres to binary
+    
+    int sock_fd = socket(AF_INET,SOCK_STREAM,0);
+    
     connect(sock_fd,(struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    
+    char msg []= "Hello Server";
     send(sock_fd,msg,strlen(msg),0);
+        
     cout<< "Messange send"<<endl;
-
 
     return 0;
 }
